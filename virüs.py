a8b8abc59ffed2e4a1f6cf2f7130f0ebe87235ef
osmancangul <75526353+osmancangul@users.noreply.github.com>
2022-07-29 14:42:25 +0300

    Add files via upload

---
+++ virüs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from turtle import *
+speed(10)
+color("cyan")
+bgcolor("black")
+b = 200
+while b > 0:
+    left(b)
+    forward(b * 3)
+    b = b - 1
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
